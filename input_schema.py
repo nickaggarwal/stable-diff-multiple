1cba5eb1a5e2ed226aac1ad21db9010ecc6f1a75
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-02-26 07:26:15 +0530

    Delete input_schema.py

--- input_schema.py
+++
@@ -1,8 +0,0 @@
-INPUT_SCHEMA = {
-    "prompt": {
-        'datatype': 'STRING',
-        'required': True,
-        'shape': [1],
-        'example': ["There is a fine house in the forest"]
-    }
-}
