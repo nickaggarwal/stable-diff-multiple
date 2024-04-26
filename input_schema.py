276f3f1efad118e2d5fba3bc0c0f500fc48dac80
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-04-26 12:27:41 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -2,7 +2,7 @@ INPUT_SCHEMA = {
     "prompt": {
         'datatype': 'STRING',
         'required': True,
-        'shape': [1],
+        'shape': [-1],
         'example': ["There is a fine house in the forest"]
     }
 }
